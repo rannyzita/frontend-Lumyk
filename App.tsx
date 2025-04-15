1e42827513612c1dfbd3f3be7c9b59d6d6e0dd15
rannyzita <fs.vitoria.soares@gmail.com>
2025-04-14 22:08:37 -0300

    testing: showing icon font awesome using svg

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Rannyzita!</Text>
+      <FontAwesomeIcon icon={faCircleUser} size={32} color="black" />
       <StatusBar style="auto" />
     </View>
   );

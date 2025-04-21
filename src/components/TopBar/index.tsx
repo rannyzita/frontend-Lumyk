0eddf8df01714bd5b31bb222bba2541af19e0885
rannyzita <fs.vitoria.soares@gmail.com>
2025-04-21 01:39:55 -0300

    Componetizando a topBar

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,41 @@
+// components/TopBar.tsx
+import React from 'react';
+import { View, TouchableOpacity, TextInput, Text, StyleSheet } from 'react-native';
+import SearchIcon from '../../assets/iconsNavigation/Icone pesquisa.svg'
+import ProfileIcon from '../../assets/iconsNavigation/Profile.svg'
+import LumykWhiteIcon from '../../assets/logoWhite.svg'; 
+import { themes } from '../../global/themes'; 
+import { styles } from './styles'; 
+
+interface TopBarProps {
+    navigation: any; // Ajuste o tipo do 'navigation' conforme necessário
+}
+
+const TopBar: React.FC<TopBarProps> = ({ navigation }) => {
+    return (
+        <View style={styles.topBar}>
+            <View style={styles.topBarContent}>
+                <View>
+                    <LumykWhiteIcon style={{ marginLeft: -5 }} />
+                </View>
+
+                <View style={styles.searchContainer}>
+                    <TouchableOpacity>
+                        <SearchIcon width={20} height={20} />
+                    </TouchableOpacity>
+                    <TextInput
+                        style={styles.searchInput}
+                        placeholder="Digite o título do livro aqui..."
+                        placeholderTextColor={themes.colors.textInput}
+                    />
+                </View>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+                    <ProfileIcon style={{ marginRight: 5 }} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+export default TopBar;

fe3e84222731f1bf6b02e39d23c226da086da19a
rannyzita <fs.vitoria.soares@gmail.com>
2025-04-21 01:22:57 -0300

    Modularizando

---
+++ src/pages/home/functions/index.tsx
@@ -0,0 +1,33 @@
+// src/functions/homeFunctions.ts
+
+export function toggleGenreSelection(
+    genre: string,
+    selectedGenres: string[],
+    setSelectedGenres: React.Dispatch<React.SetStateAction<string[]>>
+) {
+    if (selectedGenres.includes(genre)) {
+        setSelectedGenres(selectedGenres.filter(item => item !== genre));
+    } else {
+        setSelectedGenres([...selectedGenres, genre]);
+    }
+}
+
+export function toggleStateSelection(
+    state: string,
+    selectedStates: string[],
+    setSelectedStates: React.Dispatch<React.SetStateAction<string[]>>
+) {
+    if (selectedStates.includes(state)) {
+        setSelectedStates(selectedStates.filter(item => item !== state));
+    } else {
+        setSelectedStates([...selectedStates, state]);
+    }
+}
+
+export function closeDropdowns(
+    setGenreDropdownVisible: React.Dispatch<React.SetStateAction<boolean>>,
+    setStateDropdownVisible: React.Dispatch<React.SetStateAction<boolean>>
+) {
+    setGenreDropdownVisible(false);
+    setStateDropdownVisible(false);
+}

8ecc4068a0bc0aaa2a5871303ce9fe386afd7be6
rannyzita <fs.vitoria.soares@gmail.com>
2025-06-09 19:55:15 -0300

    Finalizei a tela de assinaturas

--- src/pages/subscription/styles.ts
+++ src/pages/subscription/styles.ts
@@ -1,92 +1,105 @@
 import { StyleSheet } from "react-native";
+import { themes } from "../../global/themes";
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#E6D6F2', // roxo claro de fundo
-    },
-    scroll: {
-        alignItems: 'center',
-        padding: 20,
-        paddingBottom: 100,
-    },
-    title: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        color: '#7A2EDC',
-        marginVertical: 15,
-    },
-    svgContainer: {
-        backgroundColor: '#fff',
-        borderRadius: 12,
-        padding: 10,
-        marginBottom: 20,
-        alignItems: 'center',
-        width: '100%',
-    },
-    card: {
-        backgroundColor: '#fff',
-        borderRadius: 16,
-        padding: 20,
-        marginBottom: 20,
-        width: '100%',
-        shadowColor: '#000',
-        shadowOpacity: 0.1,
-        shadowOffset: { width: 0, height: 3 },
-        shadowRadius: 4,
-        elevation: 4,
-    },
-    cardHeader: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 10,
-        borderTopWidth: 2,
-        borderTopColor: '#B57EDC',
-        paddingTop: 10,
-    },
-    planTitle: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: '#7A2EDC',
-    },
-    price: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#000',
-        marginBottom: 15,
-    },
-    benefitsContainer: {
-        marginBottom: 15,
-    },
-    benefitsTitle: {
-        fontSize: 14,
-        fontWeight: 'bold',
-        color: '#7A2EDC',
-        marginBottom: 5,
-    },
-    benefitText: {
-        fontSize: 13,
-        color: '#333',
-        marginBottom: 4,
-    },
-    button: {
-        backgroundColor: '#B57EDC',
-        paddingVertical: 10,
-        borderRadius: 8,
-        alignItems: 'center',
-    },
-    buttonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-    },
-    footer: {
-        position: 'absolute',
-        bottom: 10,
-        left: 0,
-        right: 0,
-        alignItems: 'center',
-    }
+export default StyleSheet.create({
+container: {
+    flex: 1,
+    backgroundColor: themes.colors.backgroundLumyk,
+},
+scroll: {
+    alignItems: 'center',
+    paddingVertical: 30,
+},
+AssinaturaCard: {
+    backgroundColor: '#fff',
+    width:'100%',
+    alignItems: 'center',
+    paddingVertical: 30,
+},
+title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: themes.colors.purpleDark,
+},
+svgContainer: {
+    backgroundColor: '#FFF',
+    padding: 10,
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 20,
+
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 5,
+},
+subscriptionImage: {
+    width: '95%',
+    height: 150,
+    marginLeft: 10
+},
+card: {
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    padding: 20,
+    marginBottom: 20,
+    width: '70%',
+    boxShadow: themes.colors.shadow
+},
+cardPlanos: {
+    backgroundColor: themes.colors.primary,
+    width: '117%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 8, 
+    height: 28,
+    marginLeft: -20,
+    marginRight: -20,
+},
+planTitle: {
+    fontSize: 12,
+    color: '#fff',
+},
+price: {
+    fontSize: 16,
+    marginBottom: 10,
+},
+benefitsContainer: {
+    marginBottom: 15,
+},
+benefitsTitle: {
+    fontSize: 12,
+    color: '#fff'
+},
+benefitText: {
+    fontSize: 14,
+    marginBottom: 5,
+},
+button: {
+    backgroundColor: themes.colors.primary,
+    paddingVertical: 12,
+    borderRadius: 30,
+    alignItems: 'center',
+},
+buttonText: {
+    color: '#FFF',
+    fontSize: 14,
+},
+priceRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 10,
+},
+benefitsRow: {
+    flexDirection: 'row',
+    backgroundColor: themes.colors.primary,
+    width: 120, 
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginBottom: 20,
+},
 });
-
-export default styles;

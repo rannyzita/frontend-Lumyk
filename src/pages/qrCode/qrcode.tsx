f714c6eaf3e23cfb2cbd16afdbb733ad87a796bb
rannyzita <fs.vitoria.soares@gmail.com>
2025-06-10 00:21:20 -0300

    Finalizando marromeno a tela do qr code

--- src/pages/qrCode/qrcode.tsx
+++ src/pages/qrCode/qrcode.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import QRCode from "react-native-qrcode-svg";
@@ -8,13 +8,11 @@ import NavigationHeader from "../../components/NavigationHeader/navigationHeader
 import CopyIcon from './assets/iconCopy.svg';
 import styles from './styles';
 
+import ModalFeedback from '../../components/feedbackButton/feedbackButton';
+
 export default function QrCode() {
     const pixCode = "b6f8e2c1-1234-4f9a-b234-9a9c1df...";
-
-    const handleCopy = async () => {
-        await Clipboard.setStringAsync(pixCode);
-        alert("Código Pix copiado!");
-    };
+    const [showModal, setShowModal] = useState(false);
 
     return (
         <View style={styles.container}>
@@ -27,15 +25,23 @@ export default function QrCode() {
                     </Text>
                     <View style={styles.codeRow}>
                         <Text style={styles.codeText} numberOfLines={1}>{pixCode}</Text>
-                        <TouchableOpacity onPress={handleCopy}>
+                        <TouchableOpacity onPress={() => setShowModal(true)}>
                             <CopyIcon width={24} height={24} />
                         </TouchableOpacity>
                     </View>
                 </View>
-                <TouchableOpacity style={styles.confirmButton}>
+                <TouchableOpacity style={styles.confirmButton} onPress={() => setShowModal(true)}>
                     <Text style={styles.confirmText}>CONFIRMAR</Text>
                 </TouchableOpacity>
             </View>
+
+            {showModal && (
+                <ModalFeedback
+                    title='Código copiado com sucesso'
+                    closeModal={() => setShowModal(false)}
+                    style={{ marginRight: 15 }}
+                />
+            )}
         </View>
     );
 }

0222206c9057900a5851d1d73a31cf1d2d319dbe
Kariny Leandro <karinysilva.estudo@gmail.com>
2025-07-02 10:19:39 -0300

    salvando código com banco local

--- API/index.ts
+++ API/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.7:5000', // endereço da API Python
+  baseURL: ' http://192.168.23.141:5000', // endereço da API Python
   timeout: 10000,
 });
 

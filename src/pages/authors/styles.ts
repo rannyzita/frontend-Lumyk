1529e7ec09bb0cb1289f1c2dcd394a4ecd8e1a52
rannyzita <fs.vitoria.soares@gmail.com>
2025-07-03 22:28:27 -0300

    vendo a de autores...

--- src/pages/authors/styles.ts
+++ src/pages/authors/styles.ts
@@ -1,5 +1,5 @@
-import { StyleSheet } from "react-native";
-import { themes } from "../../global/themes";
+import { StyleSheet } from 'react-native';
+import { themes } from '../../global/themes';
 
 const styles = StyleSheet.create({
     container: {

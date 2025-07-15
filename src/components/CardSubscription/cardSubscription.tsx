873864e8551e9fcc94b06dcbdd77c4434da2f215
rannyzita <fs.vitoria.soares@gmail.com>
2025-07-15 15:41:03 -0300

    Mudando a a cor do titulo de preço, ajustndo margens de altura

--- src/components/CardSubscription/cardSubscription.tsx
+++ src/components/CardSubscription/cardSubscription.tsx
@@ -16,13 +16,13 @@ const PlanCard: React.FC<PlanCardProps> = ({ id, onSelect, planSelected, faixaWi
     let benefits: string[] = [];
 
     if (id === '1') {
-        title = 'Plano 1';
+        title = 'PLANO 1';
         price = 'R$ 15,90/mês';
         benefits = [
         'Isenção do Frete, garantindo que seja cobrado apenas o valor da compra em itens.',
         ];
     } else if (id === '2') {
-        title = 'Plano 2';
+        title = 'PLANO 2';
         price = 'R$ 29,99/mês';
         benefits = [
         'Isenção do Frete, garantindo que seja cobrado apenas o valor da compra em itens.',
@@ -38,16 +38,16 @@ const PlanCard: React.FC<PlanCardProps> = ({ id, onSelect, planSelected, faixaWi
 
             <View style={styles.priceRow}>
                 <Text style={styles.price}>{price}</Text>
-                <Logo width={45} height={45} />
+                <Logo style={{marginTop: 20}} width={50} height={50} />
             </View>
 
             <View style={styles.benefitsRow}>
-                <Text style={styles.benefitsTitle}>Benefícios</Text>
+                <Text style={styles.benefitsTitle}>Benefícios:</Text>
             </View>
 
             <View style={styles.benefitsContainer}>
                 {benefits.map((item, index) => (
-                <Text key={index} style={styles.benefitText}>○ {item}</Text>
+                    <Text key={index} style={styles.benefitText}>○ {item}</Text>
                 ))}
             </View>
 
